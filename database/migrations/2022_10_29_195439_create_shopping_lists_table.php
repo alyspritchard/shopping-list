2e85cb37b16dd39b7629ae8607a2518caedcb1fb
Alys <alys.elizabeth@hotmail.com>
2022-10-29 20:29:41 +0100

    creating migrations

---
+++ database/migrations/2022_10_29_195439_create_shopping_lists_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShoppingListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shopping_lists', function (Blueprint $table) {
+            $table->id();
+            $table->integer('budget')->default(0);
+            $table->integer('total')->default(0);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shopping_lists');
+    }
+}
