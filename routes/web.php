4c1dc043d2bc0d84e23be62a1aeb21f24e5976cf
Alys <alys.elizabeth@hotmail.com>
2022-10-31 21:01:02 +0000

    creating routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ShoppingListController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +20,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'dashboard'])->middleware(['auth'])->name('dashboard');
+
+Route::prefix('/shopping-list')->middleware(['auth'])->group( function () {
+    Route::post('/store', [ShoppingListController::class, 'store'])->name("shopping-list.store");
+
+    Route::prefix('/{shopping_list_id}')->group( function () {
+        Route::put('', [ShoppingListController::class, 'update'])->name('shopping-list.update');
+        Route::delete('', [ShoppingListController::class, 'destroy'])->name('shopping-list.destroy');
+
+        Route::prefix('/items')->group ( function () {
+            Route::post('/store', [ItemController::class, 'store'])->name('item.store');
+            Route::put('/{item_id}', [ItemController::class, 'update'])->name('item.update');
+            Route::delete('/{item_id}', [ItemController::class, 'destroy'])->name('item.destroy');
+        });
+    });
+});
 
 require __DIR__.'/auth.php';
